5e5cf9afc71543cb554e77fe124f835ec5578b94
Alfonso2V <_AlfonsoV@outlook.com>
2021-10-17 13:45:47 -0500

    Updated

--- src/app/modules/favorites/favorites.module.ts
+++ src/app/modules/favorites/favorites.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FavoritesRoutingModule } from './favorites-routing.module';
-// import { PagesComponent } from './pages/pages.component';
 import { FavoritePageComponent } from './pages/favorite-page/favorite-page.component';
+import { SharedModule } from '@shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    // PagesComponent,
     FavoritePageComponent
   ],
   imports: [
     CommonModule,
-    FavoritesRoutingModule
+    FavoritesRoutingModule,
+    SharedModule
   ]
 })
 export class FavoritesModule { }
